6c11905668e84f4603989957cfa8f36199a52e70
Prafull thakur <prafullchauhan49@gmail.com>
2020-07-17 12:51:44 +0530

    added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,6 +17,15 @@ const Navbar = (props) => {
       });
   }, []);
 
+  const [showScroll, setShowScroll] = React.useState(false);
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 60) {
+      setShowScroll(true);
+    } else if (showScroll && window.pageYOffset <= 60) {
+      setShowScroll(false);
+    }
+  };
+  window.addEventListener("scroll", checkScrollTop);
   if (
     props.location.pathname === "/dashboard" ||
     props.location.pathname === "/addProduct" ||
@@ -34,168 +43,340 @@ const Navbar = (props) => {
         color: "white",
       }}
     >
-      <nav class="navbar sticky-top navbar-expand-lg navbar-light py-4">
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarTogglerDemo01"
-          aria-controls="navbarTogglerDemo01"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+      {showScroll ? (
+        <nav
+          class="navbar sticky-top navbar-expand-lg navbar-light py-4"
+          style={{ background: "#052301" }}
         >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <img src={logo} style={{ height: 26, width: 62, marginRight: 12 }} />
-
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <a class="navbar-brand" href="/" style={{ color: "white" }}>
-            Golden Pankh Export Import
-          </a>
-          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-item pl-2">
-              <a class="nav-link" href="/" style={{ color: "white" }}>
-                Home
-              </a>
-            </li>
-            <li class="nav-item pl-2">
-              <a
-                class="nav-link"
-                href="/company-profile"
-                style={{ color: "white" }}
-              >
-                Company Profile
-              </a>
-            </li>
-            <li class="nav-item dropdown pl-2">
+          {" "}
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <img src={logo} style={{ height: 26, width: 62, marginRight: 12 }} />
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="/" style={{ color: "white" }}>
+              Golden Pankh Export Import
+            </a>
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+              <li class="nav-item pl-2">
+                <a class="nav-link" href="/" style={{ color: "white" }}>
+                  Home
+                </a>
+              </li>
+              <li class="nav-item pl-2">
+                <a
+                  class="nav-link"
+                  href="/company-profile"
+                  style={{ color: "white" }}
+                >
+                  Company Profile
+                </a>
+              </li>
+              <li class="nav-item dropdown pl-2">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdownMenuLink"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                  style={{ color: "white" }}
+                >
+                  Our Products
+                </a>
+                <div
+                  class="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  {Object.keys(categories).map((cat) => {
+                    let href = "#";
+                    switch (cat) {
+                      case "Home Decorative Items":
+                        href = "/home-decor";
+                        break;
+                      case "Metal Urns":
+                        href = "/metal-urns";
+                        break;
+                      case "Christmas Decoration Items":
+                        href = "/christmas";
+                        break;
+                      case "Decorative Chandelier":
+                        href = "/decoratibe";
+                        break;
+                      case "Candle Holder":
+                        href = "/candle-holder";
+                        break;
+                      case "Metal Handicrafts":
+                        href = "/metal-handicrafts";
+                        break;
+                      case "Flower Vase":
+                        href = "/flower";
+                        break;
+                      case "MDF Frame":
+                        href = "/mdf-frame";
+                        break;
+                      case "Decorative Alphabets Letters":
+                        href = "/alphabet-items";
+                        break;
+                      case "Jewelled Mirror":
+                        href = "/jwelled-mirror";
+                        break;
+                      case "Farmstead Finials":
+                        href = "/farmstead";
+                        break;
+                      case "PPE kit, Full Body Gown, Goggles, Masks, Waste Bags":
+                        href = "/ppe-kit";
+                        break;
+                      default:
+                        href = "#";
+                        break;
+                    }
+                    return (
+                      <a class="dropdown-item" href={href}>
+                        {cat}
+                      </a>
+                    );
+                  })}
+                </div>
+              </li>
+              <li class="nav-item pl-2">
+                <a class="nav-link" href="/contact" style={{ color: "white" }}>
+                  Contact Us
+                </a>
+              </li>
+            </ul>
+            <div class="language">
               <a
                 class="nav-link dropdown-toggle"
                 href="#"
-                id="navbarDropdownMenuLink"
+                id="navbarDropdown"
                 role="button"
                 data-toggle="dropdown"
                 aria-haspopup="true"
                 aria-expanded="false"
-                style={{ color: "white" }}
               >
-                Our Products
+                Select Language <i class="fa fa-downarrow"></i>
               </a>
-              <div
-                class="dropdown-menu"
-                aria-labelledby="navbarDropdownMenuLink"
-              >
-                {Object.keys(categories).map((cat) => {
-                  let href = "#";
-                  switch (cat) {
-                    case "Home Decorative Items":
-                      href = "/home-decor";
-                      break;
-                    case "Metal Urns":
-                      href = "/metal-urns";
-                      break;
-                    case "Christmas Decoration Items":
-                      href = "/christmas";
-                      break;
-                    case "Decorative Chandelier":
-                      href = "/decoratibe";
-                      break;
-                    case "Candle Holder":
-                      href = "/candle-holder";
-                      break;
-                    case "Metal Handicrafts":
-                      href = "/metal-handicrafts";
-                      break;
-                    case "Flower Vase":
-                      href = "/flower";
-                      break;
-                    case "MDF Frame":
-                      href = "/mdf-frame";
-                      break;
-                    case "Decorative Alphabets Letters":
-                      href = "/alphabet-items";
-                      break;
-                    case "Jewelled Mirror":
-                      href = "/jwelled-mirror";
-                      break;
-                    case "Farmstead Finials":
-                      href = "/farmstead";
-                      break;
-                    case "PPE kit, Full Body Gown, Goggles, Masks, Waste Bags":
-                      href = "/ppe-kit";
-                      break;
-                    default:
-                      href = "#";
-                      break;
-                  }
-                  return (
-                    <a class="dropdown-item" href={href}>
-                      {cat}
-                    </a>
-                  );
-                })}
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">
+                  English
+                </a>
+                <a class="dropdown-item" href="#">
+                  Spanish
+                </a>
+                <a class="dropdown-item" href="#">
+                  French
+                </a>
+                <a class="dropdown-item" href="#">
+                  German
+                </a>
+                <a class="dropdown-item" href="#">
+                  Italian
+                </a>
+                <a class="dropdown-item" href="#">
+                  Chinese (Simplified)
+                </a>
+                <a class="dropdown-item" href="#">
+                  Japanese
+                </a>
+                <a class="dropdown-item" href="#">
+                  Korean
+                </a>
+                <a class="dropdown-item" href="#">
+                  Arabic
+                </a>
+                <a class="dropdown-item" href="#">
+                  Portuguese
+                </a>
               </div>
-            </li>
-            <li class="nav-item pl-2">
-              <a class="nav-link" href="/contact" style={{ color: "white" }}>
-                Contact Us
-              </a>
-            </li>
-          </ul>
-          <div class="language">
-            <a
-              class="nav-link dropdown-toggle"
-              href="#"
-              id="navbarDropdown"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Select Language <i class="fa fa-downarrow"></i>
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">
-                English
-              </a>
-              <a class="dropdown-item" href="#">
-                Spanish
-              </a>
-              <a class="dropdown-item" href="#">
-                French
-              </a>
-              <a class="dropdown-item" href="#">
-                German
-              </a>
-              <a class="dropdown-item" href="#">
-                Italian
-              </a>
-              <a class="dropdown-item" href="#">
-                Chinese (Simplified)
-              </a>
-              <a class="dropdown-item" href="#">
-                Japanese
-              </a>
-              <a class="dropdown-item" href="#">
-                Korean
-              </a>
-              <a class="dropdown-item" href="#">
-                Arabic
-              </a>
-              <a class="dropdown-item" href="#">
-                Portuguese
-              </a>
             </div>
+            <form class="form-inline my-2 my-lg-0 pl-4">
+              <input
+                class="form-control mr-sm-2"
+                type="search"
+                placeholder="Search Product/ Service"
+                aria-label="Search"
+              />
+            </form>
           </div>
-          <form class="form-inline my-2 my-lg-0 pl-4">
-            <input
-              class="form-control mr-sm-2"
-              type="search"
-              placeholder="Search Product/ Service"
-              aria-label="Search"
-            />
-          </form>
-        </div>
-      </nav>
+        </nav>
+      ) : (
+        <nav
+          class="navbar sticky-top navbar-expand-lg navbar-light py-4"
+          style={{ background: "transparent" }}
+        >
+          {" "}
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <img src={logo} style={{ height: 26, width: 62, marginRight: 12 }} />
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="/" style={{ color: "white" }}>
+              Golden Pankh Export Import
+            </a>
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+              <li class="nav-item pl-2">
+                <a class="nav-link" href="/" style={{ color: "white" }}>
+                  Home
+                </a>
+              </li>
+              <li class="nav-item pl-2">
+                <a
+                  class="nav-link"
+                  href="/company-profile"
+                  style={{ color: "white" }}
+                >
+                  Company Profile
+                </a>
+              </li>
+              <li class="nav-item dropdown pl-2">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdownMenuLink"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                  style={{ color: "white" }}
+                >
+                  Our Products
+                </a>
+                <div
+                  class="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  {Object.keys(categories).map((cat) => {
+                    let href = "#";
+                    switch (cat) {
+                      case "Home Decorative Items":
+                        href = "/home-decor";
+                        break;
+                      case "Metal Urns":
+                        href = "/metal-urns";
+                        break;
+                      case "Christmas Decoration Items":
+                        href = "/christmas";
+                        break;
+                      case "Decorative Chandelier":
+                        href = "/decoratibe";
+                        break;
+                      case "Candle Holder":
+                        href = "/candle-holder";
+                        break;
+                      case "Metal Handicrafts":
+                        href = "/metal-handicrafts";
+                        break;
+                      case "Flower Vase":
+                        href = "/flower";
+                        break;
+                      case "MDF Frame":
+                        href = "/mdf-frame";
+                        break;
+                      case "Decorative Alphabets Letters":
+                        href = "/alphabet-items";
+                        break;
+                      case "Jewelled Mirror":
+                        href = "/jwelled-mirror";
+                        break;
+                      case "Farmstead Finials":
+                        href = "/farmstead";
+                        break;
+                      case "PPE kit, Full Body Gown, Goggles, Masks, Waste Bags":
+                        href = "/ppe-kit";
+                        break;
+                      default:
+                        href = "#";
+                        break;
+                    }
+                    return (
+                      <a class="dropdown-item" href={href}>
+                        {cat}
+                      </a>
+                    );
+                  })}
+                </div>
+              </li>
+              <li class="nav-item pl-2">
+                <a class="nav-link" href="/contact" style={{ color: "white" }}>
+                  Contact Us
+                </a>
+              </li>
+            </ul>
+            <div class="language">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Select Language <i class="fa fa-downarrow"></i>
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">
+                  English
+                </a>
+                <a class="dropdown-item" href="#">
+                  Spanish
+                </a>
+                <a class="dropdown-item" href="#">
+                  French
+                </a>
+                <a class="dropdown-item" href="#">
+                  German
+                </a>
+                <a class="dropdown-item" href="#">
+                  Italian
+                </a>
+                <a class="dropdown-item" href="#">
+                  Chinese (Simplified)
+                </a>
+                <a class="dropdown-item" href="#">
+                  Japanese
+                </a>
+                <a class="dropdown-item" href="#">
+                  Korean
+                </a>
+                <a class="dropdown-item" href="#">
+                  Arabic
+                </a>
+                <a class="dropdown-item" href="#">
+                  Portuguese
+                </a>
+              </div>
+            </div>
+            <form class="form-inline my-2 my-lg-0 pl-4">
+              <input
+                class="form-control mr-sm-2"
+                type="search"
+                placeholder="Search Product/ Service"
+                aria-label="Search"
+              />
+            </form>
+          </div>
+        </nav>
+      )}
+
       <header class="pt-5 pb-4">
         <div class="motive text-center wow fadeInDown delay-3s">
           <div class="container pt-5">
@@ -230,5 +411,4 @@ const Navbar = (props) => {
     </div>
   );
 };
-
 export default withRouter(Navbar);
