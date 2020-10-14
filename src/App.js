108e747f1079396c1a0183fe85da48858d45cf5a
ayushk01 <ayushkashyap@outlook.com>
2020-10-14 22:36:03 +0530

    Finalized the Project

--- src/App.js
+++ src/App.js
@@ -4,18 +4,10 @@ import firebase from "firebase";
 // Components
 import Navigator from "./components/Navigator";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDCVVQe3g9F4EHIhAzWO18IxRBpTLVajJU",
-  authDomain: "goldenpankh-a9bc7.firebaseapp.com",
-  databaseURL: "https://goldenpankh-a9bc7.firebaseio.com",
-  projectId: "goldenpankh-a9bc7",
-  storageBucket: "goldenpankh-a9bc7.appspot.com",
-  messagingSenderId: "892495342627",
-  appId: "1:892495342627:web:ec89107fb8844d7af0d596",
-  measurementId: "G-1Z4X89LWC8",
-};
+// Tokens & Confis
+import {firebaseConfig, emailJsToken} from '../creds.js';
 
-window.emailjs.init("user_oOWTvwmYzDpjf3yINOs4X");
+window.emailjs.init(emailJsToken);
 
 firebase.initializeApp(firebaseConfig);
 
